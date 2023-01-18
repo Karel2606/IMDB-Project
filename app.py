17afe63a7102924474a462386ab9715d188101b7
Karel <karel.fischbein@st.oth-regensburg.de>
2023-01-18 01:59:49 +0100

    added basic master-detail view with sidebar to display every movie for selected actors

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ moviesDf = pd.read_csv(".\\data\\moviesOfActors.csv")
 actorsDf.index = actorsDf["Ranking"]
 st.title("Top 50 Actors 📽️🍿")
 
-topActors = actorsDf[["Ranking", "Actor Names"]].head(10)
+topActor = pd.DataFrame()
+topActors = actorsDf[["Ranking", "Actor Names"]]
+actorRankingSelected = st.sidebar.selectbox("Select Actor for Details:", topActors)
 
-st.dataframe(topActors)
+st.sidebar.dataframe(topActors[["Ranking", "Actor Names"]])
+# rendering a dataframe as an interactive table (sorting on every column possible!)
+moviesOfActorSelected = pd.DataFrame()
+moviesOfActorSelected = moviesDf[moviesDf["Actor Ranking"] == actorRankingSelected]
+# display all movies of selected actor
+actorSelected = actorsDf["Actor Names"].loc[actorRankingSelected]
+st.subheader(f"All Movies of {actorSelected}")
+st.dataframe(moviesOfActorSelected[["Title", "Year", "Genre", "Rating"]])
